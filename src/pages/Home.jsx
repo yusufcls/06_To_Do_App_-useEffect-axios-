126ba1044e668807431ac4a3dfa1e2c0d911cb18
Yusuf C <yusufcfly@gmail.com>
2022-07-02 11:01:16 +0200

    Phase_1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,29 @@
 import AddTutorial from '../components/AddTutorial';
 import TutorialList from '../components/TutorialList';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState();
+
+  const url = ' https://tutorials-api-cw.herokuapp.com/api/tutorials';
+
+  const getTutorials = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setTutorials(data);
+
+    } catch (error) {console.error();
+
+    }
+  };
+
+  //! Sadece Component mount oldugunda istek yapar
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  console.log(tutorials);
 
   return (
     <>
